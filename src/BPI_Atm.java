d2128e4a8c6abb691a8ca8c3091a02e3af6af52d
John Marc <167587949+jmSanchezzz@users.noreply.github.com>
2026-02-24 01:14:13 +0000

    commit

--- src/BPI_Atm.java
+++ src/BPI_Atm.java
@@ -1,12 +1,8 @@
 public class BPI_Atm {
     public static void main(String[] args) {
         ATMDispenseChain atmDispenser = new ATMDispenseChain();
-        int amount = 14; // Amount to be dispensed
-
-        if (amount % 2 != 0) {
-            System.out.println("Amount should be in multiples of 2.");
-        } else {
+        int amount = 160; // Amount to be dispensed
             atmDispenser.dispense(new Currency(amount));
-        }
+        
     }
 }
